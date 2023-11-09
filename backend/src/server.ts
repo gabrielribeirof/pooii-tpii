c07eb565d4f9a8d3132bf8f0b148cb420f495999
gabrielribeirof <132402355+gabrielribeirof-rubcube@users.noreply.github.com>
2023-11-09 00:02:59 -0300

    feat(backend): init routing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
 import express from "express";
 
+import { UserController } from "./controller/UserController";
+
 const app = express();
+
+const userController = new UserController();
+
+app.post("users/clients", userController.addClient);
+app.post("users/managers", userController.addManager);
 
 app.use("*", (_, response) =>
 	response.status(404).send({ message: "Not Found" }),
