44fe949b971727533625552604a2a62470c7b2e8
kay <kayannesouza14@gmail.com>
2023-11-29 19:20:15 -0300

    route game não finalizada

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,19 @@
 import express from "express";
 
 import { UserController } from "./controller/UserController";
+import { GameController } from "./controller/GameController";
 
 const app = express();
 
 const userController = new UserController();
+const gameController = new GameController();
 
 app.post("users/clients", userController.addClient);
 app.post("users/managers", userController.addManager);
+app.post("game/addGame", gameController.addGame);
+app.post("game/evaluation", gameController.addEvaluation);
+app.put("game/listing", gameController.gameListing);
+// app.put("game/listing/action", gameController.gameListingAction);//
 
 app.use("*", (_, response) =>
 	response.status(404).send({ message: "Not Found" }),
